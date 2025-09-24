662ee1084654c65f125fec8374762e48bf4284e5
Skarbek <john.skarbek@hubinternational.com>
2025-09-24 11:36:27 -0500

    Add directories and datepicker file

---
+++ formidable-extensions.php
@@ -0,0 +1,11 @@
+/*
+ * Plugin Name:       Formidable Extensions
+ * Description:       Extends Formidable Forms for OrderHubCD
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            John Skarbek
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires Plugins:  formidable-forms, formidable-forms-pro, formidable-datepicker-options
+ */
