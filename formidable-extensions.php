41fa8ea2fc6f5cd79d564eb43f1ed587de0f9f2d
Skarbek <john.skarbek@hubinternational.com>
2025-09-25 14:03:57 -0500

    Try new approach using FFs dev hook

--- formidable-extensions.php
+++ formidable-extensions.php
@@ -13,106 +13,26 @@
 
 if (!defined('ABSPATH')) exit;
 
-// Enqueue custom JS
-add_action('wp_enqueue_scripts', function() {
+add_filter( 'frm_date_field_js', function( $js_args, $field ) {
+    // Look for a relative minDate like "+7d" or "+3d"
+    if ( ! empty( $js_args['minDate'] ) && preg_match( '/\+(\d+)d/', $js_args['minDate'], $matches ) ) {
+        $days = intval( $matches[1] );
+
+        // Replace with call to our custom JS
+        // Pass the input element (this) so JS can read blackout settings
+        $js_args['minDate'] = "function(){ return calculateBusinessDayMinDate({$days}, this); }";
+    }
+
+    return $js_args;
+}, 10, 2 );
+
+add_action( 'wp_enqueue_scripts', function() {
+    // Adjust path if you put the JS in a subfolder
     wp_enqueue_script(
-        'clientsite-formidable-datepicker',
-        plugin_dir_url(__FILE__) . 'js/datepicker-mods.js',
-        ['jquery', 'jquery-ui-datepicker'],
+        'formidable-business-days', 
+        plugin_dir_url(__FILE__) . 'formidable-business-days.js',
+        array('jquery', 'jquery-ui-datepicker'), // dependencies
         '1.0.0',
-        true
+        true // load in footer
     );
 });
-
-// Add data-blackouts attribute to the wrapper
-add_filter('frm_setup_new_fields_vars', function($field, $field_obj){
-    if ($field['type'] === 'date') {
-        if (!empty($field['field_options']['unique_dates'])) {
-            $blackouts = array_map('sanitize_text_field', $field['field_options']['unique_dates']);
-            $field['container_attributes'] = isset($field['container_attributes'])
-                ? $field['container_attributes'] . ' data-blackouts="' . esc_attr(implode(',', $blackouts)) . '"'
-                : 'data-blackouts="' . esc_attr(implode(',', $blackouts)) . '"';
-        }
-    }
-    return $field;
-}, 20, 2);
-
-// // Enqueue custom scripts
-// add_action( 'wp_enqueue_scripts', function() {
-//     // Only load on pages with Formidable forms to avoid extra overhead
-//     if ( function_exists( 'frm_form_loaded' ) ) {
-//         wp_enqueue_script(
-//             'clientsite-formidable-datepicker',
-//             plugin_dir_url( __FILE__ ) . 'js/datepicker-mods.js',
-//             ['jquery', 'jquery-ui-datepicker'],
-//             '1.0.0',
-//             true
-//         );
-//     }
-// });
-
-// Enqueue custom scripts
-// add_action('wp_enqueue_scripts', function() {
-//     wp_enqueue_script(
-//         'clientsite-formidable-datepicker',
-//         plugin_dir_url(__FILE__) . 'js/datepicker-mods.js',
-//         ['jquery', 'jquery-ui-datepicker'],
-//         '1.0.0',
-//         true
-//     );
-// });
-
-// // Test forcing a blackout of dates manually
-// add_filter('frm_setup_new_fields_vars', function($field, $field_obj){
-//     if ($field['type'] === 'date') {
-//         if (!isset($field['classes'])) $field['classes'] = '';
-//         $field['classes'] .= ' frm-blackouts';
-        
-//         // Force data attribute into container
-//         $field['container_attributes'] = isset($field['container_attributes'])
-//             ? $field['container_attributes'] . ' data-blackouts="2025-10-03,2025-10-06"'
-//             : 'data-blackouts="2025-10-03,2025-10-06"';
-//     }
-//     return $field;
-// }, 20, 2);
-
-
-// // add_filter('frm_setup_new_fields_vars', function($field, $field_obj){
-// //     if ($field['type'] === 'date') {
-// //         if (!isset($field['classes'])) $field['classes'] = '';
-// //         $field['classes'] .= ' frm-blackouts';
-// //         $field['container_attributes'] = isset($field['container_attributes'])
-// //             ? $field['container_attributes'] . ' data-blackouts="2025-10-03,2025-10-06"'
-// //             : 'data-blackouts="2025-10-03,2025-10-06"';
-// //     }
-// //     return $field;
-// // }, 20, 2);
-
-
-
-// add_filter('frm_setup_new_fields_vars', function($field, $field_obj){
-//     if ($field['type'] === 'date') {
-//         // Check if the field has blackout dates set in Formidable
-//         if (!empty($field['field_options']['unique_dates'])) {
-//             $blackouts = $field['field_options']['unique_dates'];
-//             if (is_array($blackouts)) {
-//                 // Sanitize and convert to comma-separated string
-//                 $blackouts = array_map('sanitize_text_field', $blackouts);
-//                 $blackouts_str = implode(',', $blackouts);
-
-//                 // Add as data attribute to the field wrapper
-//                 if (!isset($field['classes'])) {
-//                     $field['classes'] = '';
-//                 }
-//                 $field['classes'] .= ' frm-blackouts';
-//                 // $field['container_attributes'] = isset($field['container_attributes'])
-//                 //     ? $field['container_attributes'] . ' data-blackouts="' . esc_attr($blackouts_str) . '"'
-//                 //     : 'data-blackouts="' . esc_attr($blackouts_str) . '"';
-//                 $field['container_attributes'] = isset($field['container_attributes'])
-//                     ? $field['container_attributes'] . ' ' . 'data-blackouts="' . esc_attr($blackouts_str) . '"'
-//                     : 'data-blackouts="' . esc_attr($blackouts_str) . '"';
-//             }
-//         }
-//     }
-//     return $field;
-// }, 20, 2);
